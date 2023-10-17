6d465c67287a198f5e7906b911741b3efb7e61ae
Eliezer Castro <smallcity.programmer@gmail.com>
2023-10-17 11:16:58 -0300

    desafio finalizado

---
+++ js.js
@@ -0,0 +1,9 @@
+
+
+
+function recall (cachorro, whatever, gato) {
+  let nome = `Meu nome é ${whatever} e eu tenho um ${gato} e um ${cachorro}`;
+  console.log(nome);
+}
+
+recall("cachorro", 'eliezer', 'gato');
---
+++ script.js
@@ -0,0 +1,30 @@
+  const pokemonName = document.querySelector(".pokemon-name");
+  const pokemonSprite = document.querySelector(".pokemon-sprite");
+  const pokemonId = document.querySelector(".id");
+  const button = document.querySelector(".btn-input");
+  const input = document.querySelector(".input")
+
+  const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    const data = APIResponse.json();
+    return data;
+  }
+
+
+
+  const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name.toUpperCase();
+    pokemonSprite.src = data.sprites.other["official-artwork"].front_default;
+    pokemonId.innerHTML = data.id;
+    console.log(data);
+  }  
+  renderPokemon("1")
+
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+    renderPokemon(input.value);
+  })
+
+  
