dfa978412286a0179db3a8be9d04d6b220267315
Eliezer Castro <smallcity.programmer@gmail.com>
2023-10-17 11:17:53 -0300

    atualização

--- js.js
+++
@@ -1,9 +0,0 @@
-
-
-
-function recall (cachorro, whatever, gato) {
-  let nome = `Meu nome é ${whatever} e eu tenho um ${gato} e um ${cachorro}`;
-  console.log(nome);
-}
-
-recall("cachorro", 'eliezer', 'gato');
